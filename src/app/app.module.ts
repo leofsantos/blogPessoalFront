7250c476fca92ff73293ca22ce4a7a9043e22f3a
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-05-27 23:48:03 -0300

    Implemented menu, footer, login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RegisterComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
