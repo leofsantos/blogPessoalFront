b18db9ce64ff16198a0e5e649e0cafcab6e1b335
Leonardo dos Santos <leonardo.filipe.snts@gmail.com>
2022-06-09 00:55:16 -0300

    Implemented post and get methods for Posts

---
+++ src/app/service/posts.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization',environment.token)
+  }
+
+  getAllPosts(): Observable<Postagem[]>{
+    return this.http.get<Postagem[]>('https://timetravellers.herokuapp.com/postagens/all', this.token)
+  }
+
+  postPosts(post: Postagem): Observable<Postagem>{
+    return this.http.post<Postagem>('https://timetravellers.herokuapp.com/postagens/publicar', post, this.token)
+  }
+}
